1617158634ace946f78dd016981509cb50db9e68
luoyecheng <luobolyc@163.com>
2021-03-31 15:21:09 +0800

    zhuzhu

--- src/demo/CyclicBarrierExample.java
+++ src/demo/CyclicBarrierExample.java
@@ -5,13 +5,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class CyclicBarrierExample {
     public static void main(String[] args) throws Throwable {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(1,1);
-        map.put(2,1);
-        map.put(3,1);
-        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
-        System.out.println(entries.size());
-        map.put(4,4);
-        System.out.println(entries.size());
+
     }
 }
--- src/demo/test_1.java
+++ src/demo/test_1.java
@@ -1,5 +1,6 @@
 package demo;
 import java.util.*;
+import java.math.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class test_1 {
@@ -20,18 +21,50 @@ public class test_1 {
     }
     public static ReentrantLock reentrantLock=new ReentrantLock();
     public static void main(String[] args) {
-        //[[1,15],[7,18],[7,6],[7,100],[2,200],[17,30],[17,45],[3,5],[7,8],[3,6],[3,10],[7,20],[17,3],[17,45]]
-        int[][] ans=new int[][]{{1,15},{7,18},{7,6},{7,100},{2,200},{17,30},{17,45},{3,5},{7,8},{3,6},{3,10},{7,20},{17,3},{17,45}};
-        Arrays.sort(ans, new Comparator<int[]>() {
+        PriorityQueue<Integer> pq=new PriorityQueue<>(new Comparator<Integer>(){
             @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[0]==o2[0])
-                    return o1[1]-o2[1];
-                return o1[0]-o2[0];
+            public int compare(Integer o1,Integer o2){
+                return o1-o2;
             }
         });
-        for (int[] an : ans) System.out.println(Arrays.toString(an));
+        System.out.println();
     }
+
+
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        Map<Integer, List<List<String>>> map = new HashMap<Integer, List<List<String>>>();
+        List<List<String>> wordBreaks = backtrack(s, s.length(), new HashSet<String>(wordDict), 0, map);
+        List<String> breakList = new LinkedList<String>();
+        for (List<String> wordBreak : wordBreaks) {
+            breakList.add(String.join(" ", wordBreak));
+        }
+        return breakList;
+    }
+
+    public List<List<String>> backtrack(String s, int length, Set<String> wordSet, int index, Map<Integer, List<List<String>>> map) {
+        if (!map.containsKey(index)) {
+            List<List<String>> wordBreaks = new LinkedList<List<String>>();
+            if (index == length) {
+                wordBreaks.add(new LinkedList<String>());
+            }
+            for (int i = index + 1; i <= length; i++) {
+                String word = s.substring(index, i);
+                if (wordSet.contains(word)) {
+                    List<List<String>> nextWordBreaks = backtrack(s, length, wordSet, i, map);
+                    for (List<String> nextWordBreak : nextWordBreaks) {
+                        LinkedList<String> wordBreak = new LinkedList<String>(nextWordBreak);
+                        wordBreak.offerFirst(word);
+                        wordBreaks.add(wordBreak);
+                    }
+                }
+            }
+            map.put(index, wordBreaks);
+        }
+        return map.get(index);
+    }
+
+
 }
 
 
